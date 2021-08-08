99ccc7ef0d0a9a5e8d760d2d01f2db0a7ab7afbe
abdul <abdugaffor.abdurahimov@inoviagroup.se>
2021-08-08 11:35:06 +0500

    Test

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    presets: [
-        [
-            '@babel/preset-env',
-            { targets: { node: 'current' } },
-            '@babel/preset-react',
-        ],
-        '@babel/preset-typescript',
+  presets: [
+    [
+      "@babel/preset-env",
+      { targets: { node: "current" } },
+      "@babel/preset-react",
     ],
-}
+    "@babel/preset-typescript",
+  ],
+};
